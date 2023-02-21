42c010ed80ffd78c529cda63feba4bfc46617e99
MohsinKhan007 <mohsinwaseem65@gmail.com>
2023-02-21 16:01:05 +0100

    Connecting the endpoints with the frontend and testing. struturing the design (small fixes) and adding custom css classes (removing) styling props

--- Question4/backend/backend/employee/src/main/java/com/example/employee/EmployeeController.java
+++ Question4/backend/backend/employee/src/main/java/com/example/employee/EmployeeController.java
@@ -3,23 +3,16 @@ package com.example.employee;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value="api/Employees",method= {RequestMethod.GET,RequestMethod.PUT,RequestMethod.POST})
 public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	@PostMapping(path = "/add")
+	@PostMapping(path = "/create")
 	public Employee addEmployee(@RequestBody Employee employee) {
 		return employeeService.save(employee);
 	}
